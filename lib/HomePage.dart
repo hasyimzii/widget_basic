8caf1b5a23bba6eb3b39ac2abaa35603067a168a
hasyimzii <arihasyim.zii@gmail.com>
2021-09-08 02:07:40 +0700

    Chit chat done

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,12 +4,373 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Chit Chat'),),
+      backgroundColor: Color(0xFF222831),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFF323EDD),
+        child: Icon(
+          Icons.chat,
+        ),
+      ),
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text(
+          'Chit Chat',
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // header
+                Image.asset(
+                  'assets/images/pp.png',
+                  height: 100,
+                  width: 100,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Hasyim',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+                Text(
+                  'Just wannabe myself.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xFF9D9D9D),
+                  ),
+                ),
 
-          ],
+                // main content
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF393E46),
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // friend messages
+                      Text(
+                        'Teman',
+                        style: TextStyle(
+                          color: Color(0xFFFFFFFF),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/fr1.png',
+                            width: 55,
+                            height: 55,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Irene',
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'Hai, apa kabar kamu?',
+                                style: TextStyle(
+                                  color: Color(0xFFB2B1B9),
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            'Sekarang',
+                            style: TextStyle(
+                              color: Color(0xFFB2B1B9),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/fr2.png',
+                            width: 55,
+                            height: 55,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Steve',
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'Aku ada job untukmu',
+                                style: TextStyle(
+                                  color: Color(0xFFB2B1B9),
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            '14:35',
+                            style: TextStyle(
+                              color: Color(0xFFB2B1B9),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/fr3.png',
+                            width: 55,
+                            height: 55,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Lia',
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                ' Baik, terima kasih.',
+                                style: TextStyle(
+                                  color: Color(0xFFB2B1B9),
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            '14:10',
+                            style: TextStyle(
+                              color: Color(0xFFB2B1B9),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/fr4.png',
+                            width: 55,
+                            height: 55,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Michael',
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'Besok aku akan mampir..',
+                                style: TextStyle(
+                                  color: Color(0xFFB2B1B9),
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            '10:15',
+                            style: TextStyle(
+                              color: Color(0xFFB2B1B9),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+
+                      // group messages
+                      Text(
+                        'Grup',
+                        style: TextStyle(
+                          color: Color(0xFFFFFFFF),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/gr1.png',
+                            width: 55,
+                            height: 55,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Alumni SMAN 2',
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'Kapan kita reuni?',
+                                style: TextStyle(
+                                  color: Color(0xFFB2B1B9),
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            '15:00',
+                            style: TextStyle(
+                              color: Color(0xFFB2B1B9),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/gr2.png',
+                            width: 55,
+                            height: 55,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Bani Wachid',
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                'Nanti malam makan2',
+                                style: TextStyle(
+                                  color: Color(0xFFB2B1B9),
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            '11:22',
+                            style: TextStyle(
+                              color: Color(0xFFB2B1B9),
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
